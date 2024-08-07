37ba9ae754fad978740605e74cde3271308e3da6
ltroya-as <luis.troya@allansolutions.com>
2024-08-07 13:05:54 -0500

    feat: home page

---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+  <TheLayout>
+    <div class="flex-grow">
+      <slot/>
+    </div>
+  </TheLayout>
+</template>
+
+<script>
+import TheLayout from '~/components/TheLayout.vue'
+import LightboxMixin from '~/mixins/LightboxMixin'
+
+export default {
+  components: {
+    TheLayout,
+  },
+  mixins: [LightboxMixin],
+  mounted() {
+    this.$store.commit('menu/setActiveMenu', {
+      activeMenu: null,
+      activeSubMenu: null,
+    })
+  },
+}
+</script>
